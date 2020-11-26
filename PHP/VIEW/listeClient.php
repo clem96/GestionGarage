17851d972a7cfb2ec7074115e0220c990736b051
AamandaLF <laforce.amanda@gmail.com>
2020-11-26 11:02:00 +0100

    liste client

---
+++ PHP/VIEW/listeClient.php
@@ -0,0 +1,16 @@
+<?php
+
+$listeclient = ClientsManager::getList();
+echo'<div class="titre info centre"><p>Liste des clients</p></div><br>';
+echo '<div class="info colonne">';
+echo '<div class="paire"><p><button><a href="index.php?codePage=formClient&mode=ajout">Ajouter un Client</a></button></p></div>';
+
+
+foreach ($listeclient as $unClient)
+{
+echo $unClient->getNomClient();
+echo $unClient->getPrenomClient();
+echo '<div class="impaire"><p><button><a href="index.php?codePage=formClient&mode=edit&id='.$unClient->getIdClient().'">Edition</a></button></p></div>';
+echo '<div class="paire"><p><button><a href="index.php?codePage=formClient&mode=modif&id='.$unClient->getIdClient().'">Modification</a></button></p></div>';
+echo '<div class="impaire"><p><button><a href="index.php?codePage=formClient&mode=delete&id='.$unClient->getIdClient().'">Supression</a></button></p></div>';
+}
