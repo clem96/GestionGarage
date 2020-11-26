d1f326a91c7369c0aa508b8158cb854a15ec1118
AamandaLF <laforce.amanda@gmail.com>
2020-11-26 14:35:24 +0100

    maj

---
+++ PHP/VIEW/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+echo '<nav class="displayflex centre">';
+echo '
+<button><a href="index.php?codePage=listeClient">Clients</a></button>
+<button><a href="index.php?codePage=listeVehicule">Véhicules</a></button>
+<button><a href="index.php?codePage=listePiece">Pièces</a></button>
+<button><a href="index.php?codePage=listeReparation">Réparations</a></button>
+<button><a href="index.php?codePage=listeIntervention">Interventions</a></button>
+';
+echo '</nav>';
