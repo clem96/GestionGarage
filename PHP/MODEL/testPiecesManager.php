6c6a590d3f4c98b3a8c6cc6b6a5ac119d3f4f9fb
clem96 <dubrunfautclem@outlook.fr>
2020-11-26 14:41:52 +0100

    14h40

--- PHP/MODEL/testPiecesManager.php
+++ PHP/MODEL/testPiecesManager.php
@@ -1,22 +1,34 @@
 <?php
 require 'PiecesManager.Class.php';
-require 'PiecesClass.Class.php';
+require '../CONTROLER/Pieces.Class.php';
+require 'DbConnect.Class.php';
+$db = DbConnect::init();
 
-//test d ajout
+
+//teste ajout
 $p = new Pieces(["idPiece"=>20, "libelle"=>"testlibelle", "prix"=>20]);
+var_dump($p);
 PiecesManager::add($p);
 
-//test de modif
-//$up = new Pieces(["idPiece"=>1, "libelle"=>"testmodif", "prix"=>500]);
-//PiecesManager::upDate($up);
 
-//suppression
-//$del = new Pieces(["idPiece"=>2, "libelle"=>"testmodif", "prix"=>2]);
-//PiecesManager::delete($del);
 
-//test de recherche par id
-//var_dump(PiecesManager::findById(5));
+//teste modif
+$up = new Pieces(["idPiece"=>1, "libelle"=>"testmodif", "prix"=>500]);
+PiecesManager::upDate($up);
 
-//test affiche list
-//var_dump(PiecesManager::getList());
+
+//teste suppression
+$del = new Pieces(["idPiece"=>2, "libelle"=>"testmodif", "prix"=>2]);
+PiecesManager::delete($del);
+
+
+//recherche par id
+var_dump(PiecesManager::findById(5));
+
+
+//affiche list
+$tab = PiecesManager::getList();
+foreach($tab as $piece){
+    var_dump($piece);
+}
 
