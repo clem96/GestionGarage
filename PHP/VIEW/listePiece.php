3c626adcc90b27d5f7fe1e956681d39ed598289f
clem96 <dubrunfautclem@outlook.fr>
2020-11-30 15:50:39 +0100

    30/11

--- PHP/VIEW/listePiece.php
+++ PHP/VIEW/listePiece.php
@@ -6,7 +6,7 @@ echo'<div class="info colonne">';
 foreach($liste as $piece)
 {
     $conteur++;
-    //'if($conteur%2=0){echo'.paire'}else{echo'impaire'}'
+    //'.if($conteur%2==0){echo'paire';}else{echo'impaire';.'}
 echo'<div class="paire"><p>'.$piece->getLibelle().'</p></div>';
 }
 echo'</div>';
